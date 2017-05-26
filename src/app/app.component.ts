30e25ee1decfe68b543a52770a5d13d2cf721c29
inonkp <inonkp@gmail.com>
2017-05-26 09:53:01 +0300

    implemented dragging on canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,17 +17,18 @@ import * as $ from 'jquery';
       -moz-transform: translate3d(0, 0, 0);
       -ms-transform: translate3d(0, 0, 0);
       transform: translate3d(0, 0, 0);
-      background-image: url(https://cdn.rawgit.com/Reactive-Extensions/rx.angular.js/master/examples/draganddrop/logo.png);
+      //background-image: url(https://cdn.rawgit.com/Reactive-Extensions/rx.angular.js/master/examples/draganddrop/logo.png);
       background-repeat: no-repeat;
       background-position: center;
       background-size: contain;
 	  position: absolute;
 	  left: 20px;
-      height: 200px;
-      width: 200px;
+      height: 3000;
+      width: 3000;
       color: #000000;
       border: 1px solid #666666;
-      padding: 10px;
+
+
     }
   `]
 })
@@ -37,6 +38,12 @@ export class AppComponent {
   mousemove;
   mousedown;
   draggable;
+  img;
+  canvas;
+  ctx;
+  imgX;
+  imgY;
+  
 
 
 
@@ -45,38 +52,68 @@ export class AppComponent {
 		
 
   }
+  
+  imageReady() {
+		
+		this.ctx.drawImage(this.img, 0, 0, this.img.width,    this.img.height,     // source rectangle
+                   0, 0,this.draggable.width, this.draggable.height); // destination rectangle
+//		this.img.width = '700';
+//		this.img.width = '1000';
+		console.log(this.img.width);
+		
+		this.imgX = 0;
+		this.imgY = 0;
+   }
+
 
 
   ngOnInit() {
+		this.img = new Image();
 		
+		
+		this.img.src = "https://cdn.shopify.com/s/files/1/0072/7502/products/2016-4-12_2.jpg?v=1493764875";
+		this.img.onload = (() => this.imageReady());
+		
+
+		
+		
+
 		this.mouseup = Observable.fromEvent(document, 'mouseup');
 		this.mousemove = Observable.fromEvent(document, 'mousemove');
 		this.draggable = document.getElementById("draggable");
+		this.ctx = this.draggable.getContext("2d");
 		this.mousedown = Observable.fromEvent(this.draggable, 'mousedown');
 		this.draggable.style.position = 'relative';
 		this.draggable.style.cursor = 'pointer';
-		console.log(this.draggable.getBoundingClientRect());
-		$(this.draggable).css({'left' : 20, 'top':20});
+		
+		
 		this.mousedrag = this.mousedown.map((event: any) => {
 		
 		event.preventDefault();
-			console.log( parseFloat(this.draggable.style.left));
+			//console.log( this.img);
 			return {
-			  left: event.clientX - parseFloat(this.draggable.style.left),
-			  top:  event.clientY - parseFloat(this.draggable.style.top)
+			  left: event.clientX - parseFloat(this.imgX),
+			  top:  event.clientY - parseFloat(this.imgY)
 			};
 		})
-		.flatMap(imageOffset => this.mousemove.map((pos: any) => ({
-			top:  pos.clientY - imageOffset.top,
-			left: pos.clientX - imageOffset.left
-		}))
+		.flatMap(imageOffset => this.mousemove.map((pos: any) => {
+				
+			return {
+				top:  pos.clientY - imageOffset.top,
+				left: pos.clientX - imageOffset.left
+			}
+		})
 		.takeUntil(this.mouseup));
     this.mousedrag.subscribe({
       next: pos => {
         // Update position
-		
-        this.draggable.style.top  = pos.top  + 'px';
-        this.draggable.style.left = pos.left + 'px';
+		this.ctx.clearRect(0,0,this.draggable.width,this.draggable.height);
+		this.ctx.drawImage(this.img, 0, 0, this.img.width,    this.img.height,     // source rectangle
+                   pos.left, pos.top,this.draggable.width, this.draggable.height);
+        
+		//this.ctx.drawImage(this.img, pos.left,pos.top);
+		this.imgX = pos.left;
+		this.imgY = pos.top;
       }
     });
   }
